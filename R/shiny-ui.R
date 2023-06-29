066cac28dc9ea00162fc85b10e5e5fac93230c51
Peter Solymos <psolymos@gmail.com>
2023-06-29 01:47:21 -0600

    Log heatmap dimensions
    Signed-off-by: Peter Solymos <psolymos@gmail.com>

--- R/shiny-ui.R
+++ R/shiny-ui.R
@@ -419,6 +419,8 @@ originalHeatmapOutput = function(heatmap_id, title = NULL,
 	layout = shiny_env$heatmap[[heatmap_id]]$layout
 	if(is.null(layout)) layout = ""
 
+	message("Original heatmap: H=", height, "px, W=", width, "px")
+
 	heatmap_hash = paste0("c", digest(heatmap_id, "crc32"))
     main_heatmap_ui = div(id = qq("@{heatmap_id}_heatmap_group"),
 
@@ -433,6 +435,7 @@ originalHeatmapOutput = function(heatmap_id, title = NULL,
     	# htmlOutput(qq("@{heatmap_id}_warning")),
 
 		if(identical(title, NULL) || identical(title, "")) NULL else h5(title),
+
 
 		div(id = qq("@{heatmap_id}_heatmap_resize"),
 		  if(add_spinner) {
