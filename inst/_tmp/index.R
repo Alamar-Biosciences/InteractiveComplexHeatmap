4d2c0e2f6f279d7d4345c5d2ccb98849b4a6e209
Peter Solymos <psolymos@gmail.com>
2023-08-17 19:29:53 -0600

    Experiment with label sizing Adapting the app to showcase resizing
    Signed-off-by: Peter Solymos <psolymos@gmail.com>

--- inst/_tmp/index.R
+++ inst/_tmp/index.R
@@ -4,12 +4,14 @@ library(shiny)
 
 devtools::load_all()
 
-data(rand_mat) # simply a random matrix
-ht1 = Heatmap(rand_mat, name = "mat",
-    show_row_names = TRUE, show_column_names = TRUE, row_title_rot = 0)
-ht1 = draw(ht1)
+nr <- 25
+nc <- 50
+sc <- 5
+set.seed(0)
+X <- matrix(runif(nr * nc, -1, 1), nr, nc)
+dimnames(X) <- list(paste0("r_", 1:nr), paste0("c_", 1:nc))
 
-ui = fluidPage(
+ui <- fluidPage(
     h3("My first interactive ComplexHeatmap Shiny app"),
     p("This is an interactive heatmap visualization on a random matrix."),
     InteractiveComplexHeatmapOutput(
@@ -21,8 +23,41 @@ ui = fluidPage(
         add_spinner = TRUE)
 )
 
-server = function(input, output, session) {
+server <- function(input, output, session) {
+    # ht1 <- Heatmap(X, name = "mat",
+    #     show_row_names = TRUE, show_column_names = TRUE, row_title_rot = 0,
+    #     row_names_gp = gpar(fontsize = sc),
+    #     column_names_gp = gpar(fontsize = sc))
+    ht1 <- Heatmap(X, name = "mat",
+        show_row_names = TRUE, show_column_names = TRUE, row_title_rot = 0)
     makeInteractiveComplexHeatmap(input, output, session, ht1)
 }
 
-shinyApp(ui, server)
+shinyApp(ui, server, options = list(port = 8080))
+
+
+if (FALSE) {
+# label resizing example
+library(ComplexHeatmap)
+sc <- 20
+nr <- 25
+nc <- 50
+set.seed(0)
+X <- matrix(runif(nr * nc, -1, 1), nr, nc)
+dimnames(X) <- list(paste0("r_", 1:nr), paste0("c_", 1:nc))
+ht <- Heatmap(X, name = "mat",
+    show_row_names = TRUE, show_column_names = TRUE, row_title_rot = 0,
+    row_names_gp = gpar(fontsize = sc),
+    column_names_gp = gpar(fontsize = sc))
+draw(ht)
+
+ht@row_names_param$gp$fontsize[] <- 5
+ht@column_names_param$gp$fontsize[] <- 5
+
+draw(ht)
+
+ht@row_names_param$gp$fontsize[] <- 12
+ht@column_names_param$gp$fontsize[] <- 12
+
+draw(ht)
+}
