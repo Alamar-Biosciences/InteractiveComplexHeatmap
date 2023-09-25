82a890afa0f63229b631217f7c8157b138f84eb2
Peter Solymos <psolymos@gmail.com>
2023-09-24 19:03:57 -0600

    Cleanup test script
    Signed-off-by: Peter Solymos <psolymos@gmail.com>

--- inst/_tmp/index.R
+++ inst/_tmp/index.R
@@ -4,8 +4,8 @@ library(shiny)
 
 devtools::load_all()
 
-nr <- 15
-nc <- 20
+nr <- 5
+nc <- 10
 sc <- 5
 set.seed(0)
 X <- matrix(runif(nr * nc, -1, 1), nr, nc)
@@ -14,14 +14,17 @@ dimnames(X) <- list(paste0("r_", 1:nr), paste0("c_", 1:nc))
 use_plotly=T
 
 ui <- fluidPage(
+    tags$style('.container-fluid {background-color: #ff006644;}'),
+    # tags$style("html {background-color: black}"),
     h3("My first interactive ComplexHeatmap Shiny app"),
     p("This is an interactive heatmap visualization on a random matrix."),
     InteractiveComplexHeatmapOutput(
         title1 = "", title2 = "",
-        width1 = 690, height1 = 500,
-        width2 = 290, height2 = 500,
+        width1 = 690, height1 = 400,
+        width2 = 290, height2 = 400,
         containment = TRUE,
-        output_ui_float = FALSE, layout="1-3", title3=NULL, closable_output = FALSE,
+        output_ui_float = FALSE, layout="1-3", 
+        title3=NULL, closable_output = FALSE,
         add_spinner = TRUE,
         plotly = use_plotly)
 )
@@ -36,18 +39,5 @@ server <- function(input, output, session) {
     makeInteractiveComplexHeatmap(input, output, session, ht1, plotly = use_plotly)
 }
 
+# op <- par(bg="transparent")
 shinyApp(ui, server, options = list(port = 8080))
-
-
-# .r =LETTERS[1:3] # target
-# .c = letters[1:5] # sample
-# .m <- matrix(1:15,3,5)
-# .i <- paste0("Target: ", rep(.r, length(.c)), "\nSample: ", rep(.c, each=length(.r)), "\nValue: ", as.numeric(.m))
-# dim(.i) <- dim(.m)
-
-# .f <- plotly::plot_ly(x = .c, y = .r, z = .m, type = "heatmap", hoverinfo = 'text', text = .i)
-# 					# colors = grDevices::colorRampPalette(ht_opt("COLOR"))(256),
-# 					hoverinfo = 'text', text = .i)
-
-## TODO:
-## - position is off whenever the sub heatmap group is shown
